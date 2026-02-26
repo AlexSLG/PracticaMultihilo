fa40c47b15d44b1314e0f1a71f2f0838d8052d34
AlexSLG <111134750+AlexSLG@users.noreply.github.com>
2026-02-26 18:47:24 -0500

    Add files via upload

---
+++ Server/ServerMain.java
@@ -0,0 +1,19 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+public class ServerMain {
+    private int clientnumber = 1;
+    public ServerMain() throws Exception {
+        ServerSocket server_socket = new ServerSocket(2020);
+        System.out.println("Servidor iniciado en 172.20.10.2:2020...");
+        while (true) {
+            Socket socket = server_socket.accept();
+            ServerThread server_thread = new ServerThread(socket, this);
+            Thread thread = new Thread(server_thread);
+            thread.start();
+        }
+    }
+    public synchronized int getClientNumber() { return clientnumber++; }
+    public static void main(String[] args) {
+        try { new ServerMain(); } catch (Exception e) { e.printStackTrace(); }
+    }
+}
